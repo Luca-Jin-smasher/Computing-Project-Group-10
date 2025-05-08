41e6f612ab6caaa0cdb15db7d17aeb4118101af8
Luca_Jin <59077251+Luca-Jin-smasher@users.noreply.github.com>
2025-05-08 20:08:51 +0100

    Task1

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
-from STOM_higgs_tools import *
-import numpy as np
+import STOM_higgs_tools
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def main():
-    print(generate_data(n_signals = 400))
+vals = STOM_higgs_tools.generate_data(n_signals=400)
 
 
-main()
+plt.rcParams.update({
+    'font.size': 12,
+    'font.family': 'serif',
+    'axes.labelsize': 14,
+    'axes.titlesize': 16,
+    'xtick.labelsize': 12,
+    'ytick.labelsize': 12,
+    'figure.dpi': 400
+})
+
+
+fig, ax = plt.subplots(figsize=(6, 4))
+
+
+bin_height, bin_edges = np.histogram(vals, range=[104.0, 155.0], bins=30)
+
+
+mean = (bin_edges[:-1] + bin_edges[1:]) / 2
+ystd = np.sqrt(bin_height)
+xstd = (-bin_edges[:-1] + bin_edges[1:])/2
+
+
+ax.errorbar(
+    mean, bin_height, yerr=ystd, xerr=xstd,
+    fmt='o', markersize=3, color='black',
+    ecolor='black', elinewidth=1, capsize=2
+)
+
+
+ax.set_xlim(104, 155)
+ax.set_xlabel("Rest mass/GeV")
+ax.set_ylabel("Number of entries")
+
+plt.tight_layout()
+
+plt.savefig("histogram.png", bbox_inches='tight')
+plt.show()
+
